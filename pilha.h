1dfa04678e655d22e077a84ffbbaa7d6ffb467c8
Pedro da Luz <pedro.luz@hotmail.com.br>
2018-05-17 17:05:27 -0300

    Funções básicas de pilha criadas

--- pilha.h
+++ pilha.h
@@ -5,7 +5,10 @@
 
 typedef struct pilha Pilha;
 static struct pilha {
-    Carta** cartas;
+    // Topo de pilha
+    int topo;
+    // Vetor de cartas
+    Carta* cartas[52];
 
     // Adiciona nova carta
     void (*push) (Pilha* self, Carta* carta);
@@ -13,6 +16,56 @@ static struct pilha {
     Carta* (*pop) (Pilha* self);
     // Limpa pilha
     void (*limpa) (Pilha* self);
+};
+
+void free_pilha(Pilha* pilha);
+void _pilha_push(Pilha* pilha, Carta* carta);
+Carta* _pilha_pop(Pilha* pilha);
+int isPilhaVazia(Pilha* pilha);
+int isPilhaCheia(Pilha* pilha);
+
+void free_pilha(Pilha* pilha) {
+    if (isPilhaVazia(pilha)) {
+        int i;
+        for (i = 0; i <= pilha->qtd_cartas; i++) {
+            Carta* carta = pilha->pop(pilha);
+            free_carta(carta);
+        }
+    }
+    free_pilha(pilha);
+}
+
+void _pilha_push(Pilha* pilha, Carta* carta) {
+    if (!isPilhaCheia(pilha)) {
+        pilha->topo++;
+        pilha->cartas[pilha->topo] = carta;
+        return;
+    }
+    printf("Pilha cheia!\n");
+    return;
+}
+
+Carta* _pilha_pop(Pilha* pilha) {
+    if (!isPilhaVazia(pilha)) {
+        Carta* carta = pilha->cartas[pilha->topo];
+        pilha->cartas[pilha->topo] = NULL;
+        pilha->topo--;
+        return carta;
+    }
+    printf("Pilha vazia!\n");
+    return NULL;
+}
+
+int isPilhaVazia(Pilha* pilha) {
+    if (pilha->topo == -1)
+        return 1;
+    return 0;
+}
+
+int isPilhaCheia(Pilha* pilha) {
+    if (pilha->topo == 51)
+        return 1;
+    return 0;
 }
 
 #endif // PILHA_H_INCLUDED
