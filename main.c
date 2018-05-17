344b4e3c4a096b2aa1ed3f7b7804dcd09a567a63
Pedro da Luz <pedro.luz@hotmail.com.br>
2018-05-17 17:34:18 -0300

    Testes com pilha de estoque

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 
 #include "carta.h"
+#include "pilha_estoque.h"
 
 int main()
 {
     Carta* carta = nova_carta("AC");
     carta->print(carta);
     free_carta(carta);
+
+    Pilha* pilha_estoque = cria_pilha_estoque();
+    pilha_estoque->free(pilha_estoque);
 
     printf("Hello world!\n");
     return 0;
