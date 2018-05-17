0688f24e311eb273373ecdd17453f892abace661
Pedro da Luz <pedro.luz@hotmail.com.br>
2018-05-17 17:34:09 -0300

    Início do tipo pilha de estoque

--- pilha_estoque.h
+++ pilha_estoque.h
@@ -1,6 +1,24 @@
 #ifndef PILHA_ESTOQUE_H_INCLUDED
 #define PILHA_ESTOQUE_H_INCLUDED
 
+#include "pilha.h"
+#include "carta.h"
 
+Pilha* cria_pilha_estoque(void) {
+    Pilha* pilha = (Pilha*) malloc(sizeof(Pilha));
+
+    if (!pilha) {
+        printf("Erro: Nao foi possivel alocar memoria para a pilha de estoque!\nO programa sera encerrado!\n");
+        exit(1);
+    }
+
+    pilha->topo = -1;
+    pilha->cartas = malloc(52 * sizeof(Carta*));
+
+    pilha->push = _pilha_push;
+    pilha->pop = _pilha_pop;
+    pilha->limpa = _pilha_limpa;
+    pilha->free = free_pilha;
+}
 
 #endif // PILHA_ESTOQUE_H_INCLUDED
