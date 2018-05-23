9b03e317e1cb8c7f403c4f7221badb3d4e8c12fa
Pedro da Luz <pedro.luz@hotmail.com.br>
2018-05-22 21:21:21 -0300

    Início da estrutura basica de manipulação do jogo de paciencia

--- main.c
+++ main.c
@@ -3,32 +3,14 @@
 
 #include "paciencia.h"
 
-#include "carta.h"
-#include "pilha.h"
-#include "pilha_estoque.h"
-#include "pilha_naipe.h"
-#include "pilha_descarte.h"
-#include "pilha_fileira.h"
-
 int main()
 {
-    Carta* carta = nova_carta("AC");
-    carta->print(carta);
-    free_carta(carta);
+    Paciencia* paciencia = inicializa_jogo_paciencia();
 
-    Pilha* pilha_estoque = cria_pilha_estoque();
-    imprimePilha(pilha_estoque);
-    Pilhas_Fileira* pilhas_fileira = cria_pilhas_fileira(pilha_estoque);
-    Pilhas_Naipe* pilhas_naipe = cria_pilhas_naipe();
-    Pilha* pilha_descarte = cria_pilha_descarte();
+    imprimePilhasFileira(paciencia->pilhas_fileira);
+    imprimePilha(paciencia->pilha_estoque);
 
-    imprimePilhasFileira(pilhas_fileira);
-    imprimePilha(pilha_estoque);
-
-    free_pilha(pilha_estoque);
-    free_pilhas_fileira(pilhas_fileira);
-    free_pilhas_naipe(pilhas_naipe);
-    free_pilha(pilha_descarte);
+    free_paciencia(paciencia);
 
     printf("Hello world!\n");
     return 0;
--- paciencia.h
+++ paciencia.h
@@ -8,5 +8,47 @@
 #include "pilha_descarte.h"
 #include "pilha_fileira.h"
 
+typedef struct paciencia Paciencia;
+static struct paciencia {
+    Pilha* pilha_estoque;
+    Pilhas_Fileira* pilhas_fileira;
+    Pilhas_Naipe* pilhas_naipe;
+    Pilha* pilha_descarte;
+};
+
+Paciencia* inicializa_jogo_paciencia(void);
+void free_paciencia(Paciencia* paciencia);
+
+Paciencia* inicializa_jogo_paciencia(void) {
+    Paciencia* paciencia = (Paciencia*)malloc(sizeof(Paciencia));
+
+    if (!paciencia) {
+        printf("Erro: Nao foi possivel alocar memoria inicializar o jogo!\nO programa sera encerrado!\n");
+        exit(1);
+    }
+
+    // Inicializa pilhas
+    Pilha* pilha_estoque = cria_pilha_estoque();
+    Pilhas_Fileira* pilhas_fileira = cria_pilhas_fileira(pilha_estoque);
+    Pilhas_Naipe* pilhas_naipe = cria_pilhas_naipe();
+    Pilha* pilha_descarte = cria_pilha_descarte();
+
+    // Relaciona pilhas inicializadas a estrutura paciencia
+    paciencia->pilha_estoque = pilha_estoque;
+    paciencia->pilhas_fileira = pilhas_fileira;
+    paciencia->pilhas_naipe = pilhas_naipe;
+    paciencia->pilha_descarte = pilha_descarte;
+
+    return paciencia;
+}
+
+void free_paciencia(Paciencia* paciencia) {
+    free_pilha(paciencia->pilha_estoque);
+    free_pilhas_fileira(paciencia->pilhas_fileira);
+    free_pilhas_naipe(paciencia->pilhas_naipe);
+    free_pilha(paciencia->pilha_descarte);
+
+    free(paciencia);
+}
 
 #endif // PACIENCIA_H_INCLUDED
